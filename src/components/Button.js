82a04bcf5c91728e06a5870792471e9f773b281d
Pedro Mion <pedromionbraga@gmail.com>
2023-04-24 10:23:20 -0300

    Ajeitando Button

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,9 +4,8 @@ const BasicButton = (props) => {
     
     return ( 
         <div>
-            <button className="button" style={{backgroundColor: props.backgroundColor}}>
+            <button className="button" style={{backgroundColor: props.backgroundColor, height: props.height, width: props, padding: props.padding}}>
                 <div className="buttonDiv">
-                    <img src={props.imgSrc} className="buttonImg"/>
                     <p className="buttonText" style={{fontSize: props.fontSize, lineHeigth: props.lineHeigth, color: props.color}}>{props.textContent}</p>
                 </div>
             </button>
@@ -17,7 +16,7 @@ const BasicButton = (props) => {
 const Button = (props) => {
     return (
         <div>
-            <BasicButton textContent={props.textContent} fontSize="25px" imgSrc={props.imgSrc} backgroundColor={props.backgroundColor} lineHeigth="30px" color={props.color} />
+            <BasicButton textContent={props.textContent} fontSize="25px" backgroundColor={props.backgroundColor} lineHeigth="30px" color={props.color} height={props.height} width={props.width} padding={props.padding} />
         </div>
     );
 }

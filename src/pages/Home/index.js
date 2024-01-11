8170fc2959cf4996c137b8faaa053d5d64a03b20
BRENO RANGEL <brenokrangel90@gmail.com>
2024-01-11 14:46:22 -0300

    [CAE-54] Adciona projeto na home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,17 +4,19 @@ import HeroSection from '../../components/HeroSection';
 import Navbar from '../../components/Navbar';
 import { NossoFoco } from '../../components/NossoFoco';
 import Falae from '../../components/Falae';
-
+import Projeto from '../../components/Projeto';
 
 const Home = () => {
     return (
       <>
         <Navbar/>
         <HeroSection/>
+        <Projeto/>
         <NossoFoco/>
         <Apoiadores/>
         <Falae/>
         <Footer/>
+        
       </>
     )
 }

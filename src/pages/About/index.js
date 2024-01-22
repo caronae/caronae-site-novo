57510567dd846762e3b03f59d96c30f5449e019c
BRENO RANGEL <brenokrangel90@gmail.com>
2024-01-22 12:49:17 -0300

    [CAE-43] conheca o projeto

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,8 +1,12 @@
 import Navbar from '../../components/Navbar';
+import ConhecaProjeto from '../../components/ConhecaProjeto';
 
 const About = () => {
     return (
+      <>
       <Navbar/>
+      <ConhecaProjeto/>
+      </>
     )
 }
 

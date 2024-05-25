059b6828bac5c851084bea8b02966615d143fd39
fgiongo <fgiongo@protonmail.com>
2024-05-25 11:08:16 -0300

    bugfix: corrigiu outros bugs relacionados ao escobo global de css

--- src/components/ComoOAppFunciona/index.jsx
+++ src/components/ComoOAppFunciona/index.jsx
@@ -6,28 +6,28 @@ const ComoOAppFunciona = () => {
     <div className={ styles.mainContainer }>
 
       <div className={ styles.listContainer }>
-        <h1>Como o app funciona?</h1>
-        <ol className={ styles.instructions }>
+        <h1 className= {styles.listTitle }>Como o app funciona?</h1>
+        <ol>
 
           <li>
-            <h2><span className={ styles.counter }/>Baixe o aplicativo</h2>
-            <p>
+            <h2 className={ styles.listItem }>Baixe o aplicativo</h2>
+            <p className={ styles.listText }>
               Baixe o aplicativo Caronaê na loja de aplicativos do seu
               dispositivo móvel (disponível para iOS e Android);
             </p>
           </li>
 
           <li>
-            <h2><span className={ styles.counter }/>Faça seu cadastro</h2>
-            <p>
+            <h2 className={ styles.listItem }>Faça seu cadastro</h2>
+            <p className={ styles.listText }>
               Faça o seu cadastro no aplicativo, seja como motorista ou
               passageiro, preenchendo suas informações de contato;
             </p>
           </li>
 
           <li>
-            <h2><span className={ styles.counter }/>Peça ou crie uma carona</h2>
-            <p>
+            <h2 className={ styles.listItem }>Peça ou crie uma carona</h2>
+            <p className={ styles.listText }>
               Para pedir uma carona, insira o ponto de partida e destino e o
               app sugere caronas. Para motoristas, defina o trajeto, horários
               e combine local e horário com o passageiro.
